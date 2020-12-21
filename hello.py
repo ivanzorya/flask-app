bb28e984f6659d4203e03b2f7b00a5e695216019
ivanzorya <ivanzorya@gmail.com>
2020-12-21 16:12:03 +0000

    test send message

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Kotic!'
+    return 'Hello Kotic!!!'
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data.decode('utf-8'), 'Hello Kotic!')
+        self.assertEqual(r.data.decode('utf-8'), 'Hello Kotic!!!')
 
     def test_post(self):
         r = self.app.post('/')
